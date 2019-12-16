58e6f21cbf7cdeb3402e3e0d3d93eb3246e6099d
baixc <969552017@qq.com>
2019-12-16 21:32:30 +0800

    删除无用import

--- src/main/java/com/github/thoughtliuw/Main.java
+++ src/main/java/com/github/thoughtliuw/Main.java
@@ -1,16 +1,14 @@
 package com.github.thoughtliuw;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 
-import static org.apache.http.impl.client.HttpClients.*;
+import static org.apache.http.impl.client.HttpClients.createDefault;
 
 public class Main {
     public static void main(String[] args) {
