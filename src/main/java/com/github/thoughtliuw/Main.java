f9e35a49c024660f499aff3f6d112b3d5040b6b5
baixc <969552017@qq.com>
2019-12-16 21:26:18 +0800

    第一行代码，爬取新浪首页内容 使用httpClient的get方法简单的爬取拿到新浪首页的内容,并添加一个冒烟测试

---
+++ src/main/java/com/github/thoughtliuw/Main.java
@@ -0,0 +1,27 @@
+package com.github.thoughtliuw;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import static org.apache.http.impl.client.HttpClients.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CloseableHttpClient httpclient = createDefault();
+        HttpGet httpGet = new HttpGet("http://sina.cn");
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            System.out.println(EntityUtils.toString(entity1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
