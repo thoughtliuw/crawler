fa8012cec8815c11199b5a87645fe3981424bbaa
read&write <969552017@QQ.COM>
2019-12-24 19:59:52 +0800

    Basic algorithm (#2)
    第一次重构
    之前是在一大段代码上添加一条注释，现在重构后为把它抽离成一个名字清晰易懂的方法
    将spot插件绑定到了Maven的varify阶段
    同时根据老师的代码自己又微调了一下，不修改原来的功能

--- src/main/java/com/github/thoughtliuw/Main.java
+++ src/main/java/com/github/thoughtliuw/Main.java
@@ -18,59 +18,87 @@ import static org.apache.http.impl.client.HttpClients.createDefault;
 public class Main {
     public static void main(String[] args) {
 
-        List<String> links = new ArrayList<>();
+        List<String> linkPool = new ArrayList<>();
         List<String> parsedLinks = new ArrayList<>();
 
         CloseableHttpClient httpclient = createDefault();
-        links.add("sina.cn");
-        while (!links.isEmpty()) {
-            String targetUrl = links.remove(links.size() - 1);
-            if (targetUrl.contains("\\")) {
-                targetUrl = targetUrl.replace("\\", "");
-            }
+        linkPool.add("sina.cn");
+        while (!linkPool.isEmpty()) {
+            String targetUrl = linkPool.remove(linkPool.size() - 1);
+            targetUrl = removeBackslashInUrl(targetUrl);
 
             // 查看这个URL是否已经被处理过
-            if (parsedLinks.contains(targetUrl)) {
+            if (checkIfUrlIsParsed(parsedLinks, targetUrl)) {
                 continue;
             }
 
             // 查看这个URL是否是我们想要的新闻页面
-            if (targetUrl.contains("news.sina.cn")
-                    || targetUrl.equals("sina.cn")) {
-                // 访问链接并解析
-                if (!targetUrl.contains("http")) {
-                    targetUrl = "http://" + targetUrl;
-                }
-                HttpGet httpGet = new HttpGet(targetUrl);
-                String targetHtml = null;
-                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                    System.out.println(targetUrl);
-                    System.out.println(response1.getStatusLine());
-                    HttpEntity entity1 = response1.getEntity();
-                    targetHtml = EntityUtils.toString(entity1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (isUsefulUrl(targetUrl)) {
 
-                Document document = Jsoup.parse(targetHtml);
-                List<Element> elements = document.getAllElements();
+                Document document = getAndParseUrl(httpclient, targetUrl);
 
-                elements.forEach(element -> {
-                    // 把新的a标签都放进链接池中
-                    if (element.tagName().equals("a")) {
-                        links.add(element.attr("href"));
-                    }
-                });
+                List<Element> links = document.select("a");
 
-                Element rootElement = elements.get(0);
-                Element articleElement = rootElement.selectFirst("article");
-                if (articleElement != null) {
-                    String title = articleElement.text();
-                    System.out.println(title);
-                }
+                links.stream().map(link -> link.attr("href"))
+                        .forEach(linkPool::add);
+
+                //如果是新闻页面就存入数据库，否则就什么都不做
+                storeIntoDatabaseItIsNewsPage(document);
+
             }
 
             parsedLinks.add(targetUrl);
         }
     }
+
+    private static boolean checkIfUrlIsParsed(List<String> parsedLinks, String targetUrl) {
+        return parsedLinks.contains(targetUrl);
+    }
+
+    private static String removeBackslashInUrl(String targetUrl) {
+        if (targetUrl.contains("\\")) {
+            targetUrl = targetUrl.replace("\\", "");
+        }
+        return targetUrl;
+    }
+
+    private static void storeIntoDatabaseItIsNewsPage(Document document) {
+        Element rootElement = document.getAllElements().get(0);
+        Element articleElement = rootElement.selectFirst("article");
+        if (articleElement != null) {
+            String title = articleElement.text();
+            System.out.println(title);
+        }
+    }
+
+    private static Document getAndParseUrl(CloseableHttpClient httpclient, String targetUrl) {
+        if (!targetUrl.contains("http")) {
+            targetUrl = "http://" + targetUrl;
+        }
+        HttpGet httpGet = new HttpGet(targetUrl);
+        String targetHtml = null;
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(targetUrl);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            targetHtml = EntityUtils.toString(entity1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Jsoup.parse(targetHtml);
+    }
+
+    private static boolean isUsefulUrl(String targetUrl) {
+        return isNewsPage(targetUrl)
+                || isInitPage(targetUrl);
+    }
+
+    private static boolean isInitPage(String targetUrl) {
+        return targetUrl.equals("sina.cn");
+    }
+
+    private static boolean isNewsPage(String targetUrl) {
+        return targetUrl.contains("news.sina.cn");
+    }
+
 }
