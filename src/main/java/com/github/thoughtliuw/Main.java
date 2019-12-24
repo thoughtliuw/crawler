6d52ab700f197479c23b27ab2bf657ca91f62c7f
baixc <969552017@qq.com>
2019-12-24 17:28:54 +0800

    第一次重构 之前是在一大段代码上添加一条注释，现在重构后为把它抽离成一个名字清晰易懂的方法

--- src/main/java/com/github/thoughtliuw/Main.java
+++ src/main/java/com/github/thoughtliuw/Main.java
@@ -25,47 +25,25 @@ public class Main {
         links.add("sina.cn");
         while (!links.isEmpty()) {
             String targetUrl = links.remove(links.size() - 1);
-            if (targetUrl.contains("\\")) {
-                targetUrl = targetUrl.replace("\\", "");
-            }
+            targetUrl = removeBackslashInUrl(targetUrl);
 
             // 查看这个URL是否已经被处理过
-            if (parsedLinks.contains(targetUrl)) {
+            if (checkIfUrlIsParsed(parsedLinks, targetUrl)) {
                 continue;
             }
 
             // 查看这个URL是否是我们想要的新闻页面
-            if (targetUrl.contains("news.sina.cn")
-                    || targetUrl.equals("sina.cn")) {
-                // 访问链接并解析
-                if (!targetUrl.contains("http")) {
-                    targetUrl = "http://" + targetUrl;
-                }
-                HttpGet httpGet = new HttpGet(targetUrl);
-                String targetHtml = null;
-                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                    System.out.println(targetUrl);
-                    System.out.println(response1.getStatusLine());
-                    HttpEntity entity1 = response1.getEntity();
-                    targetHtml = EntityUtils.toString(entity1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (isUsefulUrl(targetUrl)) {
 
-                Document document = Jsoup.parse(targetHtml);
+                Document document = parseUrlIfUseful(httpclient,targetUrl);
+
                 List<Element> elements = document.getAllElements();
 
-                elements.forEach(element -> {
-                    // 把新的a标签都放进链接池中
-                    if (element.tagName().equals("a")) {
-                        links.add(element.attr("href"));
-                    }
-                });
+                putLinkIntoLinks(links, elements);
 
-                Element rootElement = elements.get(0);
-                Element articleElement = rootElement.selectFirst("article");
-                if (articleElement != null) {
-                    String title = articleElement.text();
+                String title = getNewsData(elements);
+
+                if(title != null) {
                     System.out.println(title);
                 }
             }
@@ -73,4 +51,66 @@ public class Main {
             parsedLinks.add(targetUrl);
         }
     }
+
+    private static boolean checkIfUrlIsParsed(List<String> parsedLinks, String targetUrl) {
+        return parsedLinks.contains(targetUrl);
+    }
+
+    private static String removeBackslashInUrl(String targetUrl) {
+        if (targetUrl.contains("\\")) {
+            targetUrl = targetUrl.replace("\\", "");
+        }
+        return targetUrl;
+    }
+
+    private static String getNewsData(List<Element> elements) {
+        String title = null;
+        Element rootElement = elements.get(0);
+        Element articleElement = rootElement.selectFirst("article");
+        if (articleElement != null) {
+            title = articleElement.text();
+        }
+        return title;
+    }
+
+    private static void putLinkIntoLinks(List<String> links, List<Element> elements) {
+        elements.forEach(element -> {
+            // 把新的a标签都放进链接池中
+            if (element.tagName().equals("a")) {
+                links.add(element.attr("href"));
+            }
+        });
+    }
+
+    private static Document parseUrlIfUseful(CloseableHttpClient httpclient, String targetUrl) {
+        if (!targetUrl.contains("http")) {
+            targetUrl = "http://" + targetUrl;
+        }
+        HttpGet httpGet = new HttpGet(targetUrl);
+        String targetHtml = null;
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(targetUrl);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            targetHtml = EntityUtils.toString(entity1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Document document = Jsoup.parse(targetHtml);
+        return document;
+    }
+
+    private static boolean isUsefulUrl(String targetUrl) {
+        return isNewsPage(targetUrl)
+                || isInitPage(targetUrl);
+    }
+
+    private static boolean isInitPage(String targetUrl) {
+        return targetUrl.equals("sina.cn");
+    }
+
+    private static boolean isNewsPage(String targetUrl) {
+        return targetUrl.contains("news.sina.cn");
+    }
+
 }
