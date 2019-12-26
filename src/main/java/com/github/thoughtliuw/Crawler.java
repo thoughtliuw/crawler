fb544e62f8bfcc1f22a154338c47b02d68683f44
baixc <969552017@qq.com>
2019-12-26 21:49:30 +0800

    第二次重构，抽离模块 第一次重构抽取了方法，这一次重构将和数据库有关的操作抽离出来成为一个类， 将原来的静态方法都修改为实例方法 同时之前的代码有些bug，已处理链接池未加入数据

---
+++ src/main/java/com/github/thoughtliuw/Crawler.java
@@ -0,0 +1,112 @@
+package com.github.thoughtliuw;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.apache.http.impl.client.HttpClients.createDefault;
+
+public class Crawler {
+
+    private Dao dao = new Dao();
+
+    public void run() throws SQLException, IOException {
+        CloseableHttpClient httpclient = createDefault();
+
+        String targetUrl;
+        while ((targetUrl = dao.getNextLinkAndDelete()) != null) {
+
+            // 查询数据库中是否已经处理过这条数据
+            if (dao.checkIfUrlIsParsed("select * from LINKS_ALREADY_PROCESSED where link = ?")) {
+                continue;
+            }
+
+            // 查看这个URL是否是我们想要的新闻页面
+            if (isUsefulUrl(targetUrl)) {
+
+                Document document = getAndParseUrl(httpclient, targetUrl);
+
+                // 把文档中的a标签的href都存入数据库
+                parseLinksAndStoreIntoDBLinks(document);
+
+                //如果是新闻页面就存入数据库的新闻表中，否则就什么都不做
+                storeIntoDatabaseItIsNewsPage(targetUrl, document);
+
+            }
+
+            // 向数据库已处理链接池中插入数据
+            dao.updateDatabase(targetUrl, "insert into LINKS_ALREADY_PROCESSED values(?)");
+        }
+    }
+
+    public void storeIntoDatabaseItIsNewsPage(String url, Document document) throws SQLException {
+        Element rootElement = document.getAllElements().get(0);
+        Element articleElement = rootElement.selectFirst("article");
+        if (articleElement != null) {
+            String title = articleElement.selectFirst("h1").text();
+            ArrayList<Element> contentEls = articleElement.selectFirst(".art_content").select(".art_p");
+            String content = contentEls.stream().map(Element::text).collect(Collectors.joining("\n"));
+            dao.storeNewsIntoDataBase(url, title, content);
+        }
+    }
+
+    private String removeBackslashInUrl(String targetUrl) {
+        if (targetUrl.contains("\\")) {
+            targetUrl = targetUrl.replace("\\", "");
+        }
+        return targetUrl;
+    }
+
+    private Document getAndParseUrl(CloseableHttpClient httpclient, String targetUrl) throws IOException {
+        HttpGet httpGet = new HttpGet(targetUrl);
+        String targetHtml;
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(targetUrl);
+            HttpEntity entity1 = response1.getEntity();
+            targetHtml = EntityUtils.toString(entity1);
+        }
+        return Jsoup.parse(targetHtml);
+    }
+
+    private boolean isUsefulUrl(String targetUrl) {
+        return isNewsPage(targetUrl)
+                || isInitPage(targetUrl);
+    }
+
+    private boolean isInitPage(String targetUrl) {
+        return targetUrl.equals("sina.cn");
+    }
+
+    private boolean isNewsPage(String targetUrl) {
+        return targetUrl.contains("news.sina.cn");
+    }
+
+    public void parseLinksAndStoreIntoDBLinks(Document document) throws SQLException {
+        List<Element> links = document.select("a");
+
+        // 将链接放入待处理数据库链接池中
+        for (Element link : links) {
+
+            String href = link.attr("href");
+            String url = href;
+            if (url.startsWith("//")) {
+                url = "https:" + href;
+                url = removeBackslashInUrl(url);
+            }
+            if (!href.startsWith("javascript")) {
+                dao.updateDatabase(url, "insert into LINKS_TO_BE_PROCESSED values(?)");
+            }
+        }
+    }
+}
